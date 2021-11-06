8d5a9a1c2bbdc227896efdef7f0f817b86dae51e
KAVNSK <93428507+KAVNSK@users.noreply.github.com>
2021-11-06 17:38:27 +0700

    Написал первое разветвление, но криво

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-API_TOKEN = 'YOUR_TOKEN_SHOULD_BE_HERE'  # TODO: Вставьте сюда API_TOKEN вашего бота
+API_TOKEN = '2076254824:AAFslPJM3qqwXbktFrO78fab-B6PAXK9GJU'  # TODO: Вставьте сюда API_TOKEN вашего бота
--- run_bot.py
+++ run_bot.py
@@ -3,9 +3,7 @@ from config import API_TOKEN
 
 bot = telebot.TeleBot(API_TOKEN)
 controller = {}
-adsadasd=10
 INVALID_CHOICE = "Введите, пожалуйста, другой вариант - одно число из списка вариантов выше."
-
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.from_user.id,
@@ -29,21 +27,27 @@ def start(message):
     user_id = message.from_user.id
     user_choice = message.text
     user_state = controller.get(user_id, 'start') # Если вдруг такой user_id не сохранен, то считаем, что статус = start
-    #answer = 'none'
+    answer = 'none'
     if user_state == 'start':
         answer = start_handler(user_id, user_choice)
     if user_state == 'teacher':
         answer = teacher_handler(user_id, user_choice)
     bot.send_message(message.from_user.id, answer)
-
-
+    if user_state == 'opposite':
+        answer = opposite_handler(user_id, user_choice)
+    bot.send_message(message.from_user.id, answer)
 def start_handler(user_id, user_choice):
     if user_choice == "1":
         pass
     if user_choice == "2":
         pass
     if user_choice == "3":
-        pass
+        controller[user_id] = 'opposite'
+        return """
+                    Вы открываете глаза. Звенит будильник.
+                    [1] Выключите будильник и продолжите спать
+                    [2] Закрыть глаза и лечь на ковёр
+                    """
     if user_choice == "4":
         pass    
     if user_choice == "5":
@@ -56,6 +60,30 @@ def start_handler(user_id, user_choice):
             [2] Войдете в Zoom и начнете урок
             """
     return INVALID_CHOICE
+
+def opposite_handler(user_id, user_choice):
+    if user_choice == "1":
+        controller[user_id] = 'start'
+        return """
+                    Перед вами опять возникла приборная панель с четырьмя кнопками.
+                    На какую вы нажмете в этот раз? Введите одно число.
+                    [1] Кнопка "Ваше домашнее животное" 
+                    [2] Кнопка "Вы на 20 лет старше"
+                    [3] Кнопка "Полная противоположность вас"
+                    [4] Кнопка "Известная личность"
+                    [5] Кнопка "Самый счастливый человек на Земле"
+                    [6] Кнопка "Ваш преподаватель по проектному программированию"
+                    """
+    if user_choice == "2":
+        return """
+            Перед вами возникла делема: поесть, умыться или продолжить спать?
+            [1] Кнопка "Поползти на кухню"
+            [2] Кнопка "Поползти в ванную комнату"
+            [3] Кнопка "Вернуться в постель"
+            """
+
+    return INVALID_CHOICE
+
 
 
 def teacher_handler(user_id, user_choice):
